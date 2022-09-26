753869a7ade9e2d0ba1972c0dbe18f00b07848d4
youngmoo99 <90688872+youngmoo99@users.noreply.github.com>
2022-09-26 17:51:03 +0900

    Add files via upload

---
+++ javabasic/src/javabasic/Hi.java
@@ -0,0 +1,13 @@
+package javabasic;
+
+public class Hi {
+
+	public static void main(String[] args) {
+		// 나의 첫 자바 
+		System.out.println("Hello World!");
+		System.out.println("안녕 4교시");
+		
+		//실행 ctrl f11 
+	}
+
+}
---
+++ javabasic/src/pratice/Me.java
@@ -0,0 +1,13 @@
+package pratice;
+
+public class Me {
+
+	public static void main(String[] args) {
+		System.out.println("안녕하세요");
+		System.out.println("제 이름은");
+		System.out.println("김영무 입니다");
+		System.out.println("반갑습니다");
+
+	}
+
+}
